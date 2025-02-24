84fd11a1fd3920d267ced26c2eb60e76d11cdcdb
Adria Lama <adria.lama@enti.cat>
2025-02-24 20:06:31 +0100

    Doble Salto

--- Assets/Scripts/MovimientoPJ.cs
+++ Assets/Scripts/MovimientoPJ.cs
@@ -7,12 +7,9 @@ public class MovimientoPJ : MonoBehaviour
 
     public float movSpeed;
     public float horizontal;
-    public bool isGround;
-    public bool isJumping = false;
-    public GameObject floor;
-    public static int jumpCount;
     public float jumpSpeed;
     Rigidbody2D rb2D;
+    private bool canDoubleJump;
 
     void Start()
     {
@@ -24,9 +21,26 @@ public class MovimientoPJ : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(horizontal, 0, 0) * movSpeed * Time.deltaTime;
 
-        if (Input.GetButtonDown("Jump") && checkGround.isGround)
+        if (Input.GetButtonDown("Jump"))
         {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+            if (checkGround.isGround)
+            {
+                canDoubleJump = true;
+                rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+            }
+            else
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    if (canDoubleJump)
+                    {
+                        rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+                        canDoubleJump = false;
+                    }
+                }
+            }
         }
+
+        
     }
 }
