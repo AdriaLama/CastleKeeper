c08f7622b11f0ee059d22972c3ba1425736fb3ab
AdriaLama <adria.lama@enti.cat>
2025-03-18 12:19:22 +0100

    pick items y hookcd

---
+++ Assets/Scripts/PickItems.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickItems : MonoBehaviour
+{
+    public bool hasHook;
+    private bool hasGrab;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hook"))
+        {
+            hasHook = true;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Grab"))
+        {
+            hasGrab = true;
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/hook.cs
+++ Assets/Scripts/hook.cs
@@ -5,76 +5,76 @@ using UnityEngine;
 public class hook : MonoBehaviour
 {
     LineRenderer line;
-    private MovimientoPJ mov;
     private Rigidbody2D rb2D;
+    private PickItems pi;
     public LayerMask grapplableMask;
     public float maxDistance;
     public float grappleSpeed;
     public float grappleShootSpeed;
     public bool isGrappling = false;
     public bool retracting = false;
-    public int hookCD;
-    public bool canGrapple = true;
+    public float hookCD = 1.5f; // Ajusta el cooldown a un valor razonable
+    private bool canHook = true;
     Vector2 target;
+
     void Start()
     {
         line = gameObject.GetComponent<LineRenderer>();
-        mov = GetComponent<MovimientoPJ>();
         rb2D = GetComponent<Rigidbody2D>();
+        pi = GetComponent<PickItems>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E) && !isGrappling && canGrapple) 
+        if (Input.GetKeyDown(KeyCode.E) && !isGrappling && canHook && pi.hasHook)
         {
-            StartGrapple();
-        
+            StartCoroutine(StartGrapple());
         }
+
         if (retracting)
         {
-            Vector2 grapplePos = Vector2.Lerp(transform.position, target, grappleSpeed * Time.deltaTime);                      
+            Vector2 grapplePos = Vector2.Lerp(transform.position, target, grappleSpeed * Time.deltaTime);
             transform.position = grapplePos;
             line.enabled = false;
-
-      
 
             if (Vector2.Distance(transform.position, target) < 1f)
             {
                 retracting = false;
                 isGrappling = false;
-                line.enabled = false;          
-                rb2D.constraints = RigidbodyConstraints2D.None; 
+                line.enabled = false;
+                rb2D.constraints = RigidbodyConstraints2D.None;
                 rb2D.constraints = RigidbodyConstraints2D.FreezeRotation;
-
-
             }
-
-
         }
     }
+
     private IEnumerator StartGrapple()
-    { 
-       Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-    
+    {
+        canHook = false; 
+
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, maxDistance, grapplableMask);
-    
+
         if (hit.collider != null)
         {
             isGrappling = true;
             target = hit.point;
             line.enabled = true;
             line.positionCount = 2;
-            rb2D.velocity = Vector2.zero; 
+            rb2D.velocity = Vector2.zero;
             rb2D.constraints = RigidbodyConstraints2D.FreezePositionX;
             StartCoroutine(Grapple());
-            canGrapple = false;
-            yield return new WaitForSeconds(hookCD);
-            canGrapple = true;
-
         }
-    
+        else
+        {
+            canHook = true; 
+        }
+
+        yield return new WaitForSeconds(hookCD);
+        canHook = true; 
     }
+
     IEnumerator Grapple()
     {
         float t = 0;
@@ -94,7 +94,5 @@ public class hook : MonoBehaviour
         }
         line.SetPosition(1, target);
         retracting = true;
-
-        
     }
 }
