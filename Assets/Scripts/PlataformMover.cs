f4040cf7fc89b6a3c2d28c00947102b93ab63027
AdriaLama <adria.lama@enti.cat>
2025-05-20 12:10:34 +0200

    Plataformas CD

--- Assets/Scripts/PlataformMover.cs
+++ Assets/Scripts/PlataformMover.cs
@@ -8,6 +8,9 @@ public class PlatformMover : MonoBehaviour
     public Transform puntoA, puntoB;
     private float velocidad0;
     public float velocidadDefault;
+    public float moveCD = 2.5f;
+    public bool canMove = true;
+    public bool waiting = false;
 
     private Vector3 destino;
 
@@ -15,16 +18,27 @@ public class PlatformMover : MonoBehaviour
     {
         velocidad0 = velocidadDefault;
         destino = puntoB.position;
+        canMove = true;
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, destino, velocidad0 * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, destino) < 0.1f)
+        if (canMove)
         {
-            destino = (destino == puntoA.position) ? puntoB.position : puntoA.position;
+            transform.position = Vector3.MoveTowards(transform.position, destino, velocidad0 * Time.deltaTime);
         }
+       
+
+        if (Vector3.Distance(transform.position, destino) < 0.1f && !waiting)
+        {
+
+            StartCoroutine(plataformaCD());
+
+           
+        }
+
+        
+
     }
 
   
@@ -44,5 +58,18 @@ public class PlatformMover : MonoBehaviour
         {
             collision.transform.SetParent(null);
         }
+    }
+
+    private IEnumerator plataformaCD()
+    {
+        canMove = false;
+        waiting = true;
+        yield return new WaitForSeconds(moveCD);
+
+        destino = (destino == puntoB.position) ? puntoA.position : puntoB.position;
+
+        canMove = true;
+        waiting = false;
+
     }
 }
